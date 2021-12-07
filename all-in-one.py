0c1b74b20cadb16cb794bc447f7d428649566258
Hackers United <hackers1united@gmail.com>
2021-12-07 04:01:53 +0100

    Add files via upload

---
+++ all-in-one.py
@@ -0,0 +1,185 @@
+import time as t
+import sys
+import os
+t.sleep(3)
+os.system("clear")
+
+print("""
+
++-+-+-+-+-+-+-+-+-
+|T|E|N|I|T|E|N|O|1
++-+-+-+-+-+-+-+-+-
+
+""" )
+
+print("""\033[1;96m]
+Tool name: All in One Tool Installer
+Version: 1.0
+Creator: Teniola Ayodele
+Team: Hackers United
+Github: https://github.com/Hackers-Unit
+WhatsApp: +2348164064452
+Credit: Spider Anongreyhat
+
+""")
+
+print("""\n\033[1;92m[\033[1;94m
+0. Exit
+1. Install Anonphisher   
+2. Install Botel      
+3. install W-HACK     
+4. Install Metasploit-framework    
+5. Install Admin-finder  
+6. Install Zip-btute   
+7. Install CamOver    
+8. Install FBTOOL    
+9. Install  Trackip
+10. Install QRGen
+11. Install PhoneInfoga
+12. Install GalleryHack
+13. Install Crack-rar
+14. Install RouterSploit
+15. Install Passmaker
+16. Install CCTV-HACKING
+17. Install SocialBox-Termux
+18. Install Get-instagram-users-info---Any-instagram-account
+19. Install Sqlmap-dev
+20. Install HashCode
+21. Install Termshark
+22. Install NMap
+23. Install Malicious
+24. Install Camphish
+25. Install Skiptracer
+26. Install Osint-framework
+27. Install WhatsAppMonitor
+28. Install Pdfencrypt
+29. Install Evil-Droid
+30. Install Zphisher
+31. Install Hackers United Calculator
+32. Install HULK DoS
+33. Install WeeMan
+34. Install FB Shield
+35. Install Hackers UN Virus Maker
+""")
+
+print("\x1b[1;92mChoose a tool to install")
+t = input("\x1b[1;92m~#: ")
+os.system("clear")
+if t == "0":
+  exit()
+elif t == "1":
+  os.system("""cd
+  git clone https://github.com/TermuxHackz/anonphisher""")
+elif t == "2":
+  os.system("""cd
+  git clone https://github.com/spider863644/Botel""")
+elif t == "3":
+  os.system("""cd
+  git clone https://github.com/spider863644/W-HACK""")
+elif t == "4":
+  os.system("""cd
+  git clone https://github.com/TermuxHackz/Metasploit-framework""")
+elif t == "5":
+  os.system("""cd
+  git clone https://github.com/TermuxHackz/admin-finder""")
+elif t == "6":
+  os.system("""cd
+  git clone https://github.com/TermuxHackz/zip-brute""")
+elif t == "7":
+  os.system("""cd
+  git clone https://github.com/EntySec/CamOver""")
+elif t == "8":
+  os.system("""cd
+  git clone https://github.com/mkdirlove/FBTOOL""")
+elif t == "9":
+  os.system("""cd
+  git clone https://github.com/TermuxHackz/Trackip""")
+elif t == "10":
+  os.system("""cd
+  git clone https://github.com/h0nus/QRGen""")
+elif t == "11":
+  os.system("""cd
+  git clone https://github.com/sundowndev/PhoneInfoga""")
+elif t == "12":
+  os.system("""cd
+  git clone https://github.com/RazorKenway/GalleryHack.git""")
+elif t == "13":
+  os.system("""cd
+  git clone https://github.com/TermuxHackz/Crack-rar""")
+elif t == "14":
+  os.system("""cd
+  git clone https://github.com/reverse-shell/routersploit""")
+elif t == "15":
+  os.system("""cd
+  git clone https://github.com/Anontemitayo/Passmaker""")
+elif t == "16":
+  os.system("""cd
+  git clone https://github.com/U-danbaiwa/CCTV-HACKING.git""")
+elif t == "17":
+  os.system("""cd
+  git clone https://github.com/samsesh/SocialBox-Termux.git""")
+elif t == "18":
+  os.system("""cd
+  git clone https://github.com/zxllkada/Get-instagram-users-info---Any-instagram-account.git""")
+elif t == "19":
+  os.system("""cd
+  git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev""")
+elif t == "20":
+  os.system("""cd
+  git clone https://github.com/Sup3r-Us3r/HashCode.git""")
+elif t == "21":
+  os.system("""cd
+  git clone https://github.com/gcla/termshark""")
+elif t == "22":
+  os.system("apt install nmap")
+elif t == "23":
+  os.system("""cd
+  git clone https://github.com/Hider5/Malicious""")
+elif t == "24":
+  os.system("""cd
+  git clone https://github.com/techchipnet/CamPhish""")
+elif t == "25":
+  os.system("""cd
+  git clone https://github.com/xiwill/Skip tracer""")
+elif t == "26":
+  os.system("""cd
+  git clone https://github.com/lockfale/osint-framework""")
+elif t == "27":
+  os.system("""cd
+  git clone https://github.com/ErikTschierschke/WhatsappMonitor.git""")
+elif t == "28":
+  os.system("""cd
+  git clone https://github.com/Anontemitayo/Pdfencrypt""")
+elif t == "29":
+  os.system("""cd
+  git clone https://github.com/M4sc3r4n0/Evil-Droid""")
+elif t == "30":
+  os.system("""cd
+  git clone https://github.com/htr-tech/zphisher.git""")
+elif t == "31":
+  os.system("""cd
+  git clone https://github.com/Hackers-Unit/Calculator""")
+elif t == "32":
+  os.system("""cd
+  git clone https://github.com/grafov/hulk""")
+elif t == "33":
+  os.system("""cd
+  git clone https://github.com/evait-security/weemam""")
+elif t == "34":
+  os.system("""cd
+  git clone https://github.com/jaxBCD/FBshield""")
+elif t == "35":
+  os.system("""cd
+  git clone https://github.com/Hackers-Unit/HackersUN""")
+
+else:
+  print("\033[1;91mIncorrect Value, Choose a valid tool to install!"),os.system("python3 all-in-one.py")
+t2 = input("Do you wanna install another tools? Y/N: ")
+if t2 == "Y":
+  os.system("python3 all-in-one.py")
+elif t2 == "N":
+  exit()
+if t2 == "y":
+  os.system("python3 all-in-one.py")
+elif t2 == "n":
+  exit()
